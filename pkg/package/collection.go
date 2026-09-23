aafe4937a207e2a30148b9b12cde4f52359dd3ad
Daniele Rondina <geaaru@macaronios.org>
2026-09-23 18:12:19 +0200

    collection: Add new helpers

--- pkg/package/collection.go
+++ pkg/package/collection.go
@@ -6,6 +6,8 @@ package pkg
 
 import (
 	"fmt"
+
+	gentoo "github.com/geaaru/pkgs-checker/pkg/gentoo"
 )
 
 func (c *Collection) GetPackage(pkgName, version string) (*DefaultPackage, error) {
@@ -16,4 +18,24 @@ func (c *Collection) GetPackage(pkgName, version string) (*DefaultPackage, error
 	}
 
 	return nil, fmt.Errorf("Package %s-%s not in collection", pkgName, version)
+}
+
+func (c *Collection) GetPackageFromString(pkgstr string) (*DefaultPackage, error) {
+	gpkg, err := gentoo.ParsePackageStr(pkgstr)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.GetPackageFromGentooPkg(gpkg)
+}
+
+func (c *Collection) GetPackageFromGentooPkg(g *gentoo.GentooPackage) (*DefaultPackage, error) {
+	for idx := range c.Packages {
+		p, _ := c.Packages[idx].ToGentooPackage()
+		if matchedPkg, _ := p.Equal(g); matchedPkg {
+			return &c.Packages[idx], nil
+		}
+	}
+
+	return nil, fmt.Errorf("package %s not found in collection", g.GetPVR())
 }
