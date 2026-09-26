5d55953bbeb437ba82534c9259b7454549c22d0a
Daniele Rondina <geaaru@macaronios.org>
2026-09-26 19:10:56 +0200

    cmd/migrate-luet: Add review of config and repositories files

--- cmd/migrate-luet.go
+++ cmd/migrate-luet.go
@@ -7,10 +7,14 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	config "github.com/macaroni-os/anise/pkg/config"
 	fileHelper "github.com/macaroni-os/anise/pkg/helpers/file"
 	. "github.com/macaroni-os/anise/pkg/logger"
+	pkg "github.com/macaroni-os/anise/pkg/package"
+	installer "github.com/macaroni-os/anise/pkg/v2/installer"
+	wagon "github.com/macaroni-os/anise/pkg/v2/repository"
 
 	. "github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
@@ -67,12 +71,53 @@ func newMigrateLuetCommand(cfg *config.AniseConfig) *cobra.Command {
 				return
 			}
 
+			// Search installed packages related to "repository"
+			// category in order to rename the files list
+
 			if dir2rm {
 				err := os.RemoveAll(oldCachedir)
 				if err != nil {
 					Fatal(fmt.Sprintf(
 						"error on remove directory %s: %s",
 						oldCachedir, err.Error()))
+				}
+			}
+
+			// Check if /etc/luet is a link to /etc/anise
+			luetLink := fileHelper.ExistsLink("/etc/luet")
+			if luetLink {
+				err := os.RemoveAll("/etc/luet")
+				if err != nil {
+					Fatal(fmt.Sprintf(
+						"error on remove link /etc/luet: %s",
+						err.Error()))
+				}
+
+				// Try to remove also /etc/anise/luet.yaml link
+				os.RemoveAll("/etc/anise/luet.yaml")
+			}
+
+			err := updateRepositoriesPkgs(cfg)
+			if err != nil {
+				Fatal(fmt.Sprintf(
+					"error on update installed repositories packages: %s",
+					err.Error()))
+			}
+
+			// Rewrite paths in the config file.
+			if fileHelper.Exists("/etc/anise/anise.yaml") {
+				err = rewriteConfigPaths("/etc/anise/anise.yaml")
+				if err != nil {
+					Fatal(fmt.Sprintf(
+						"error on update file /etc/anise/anise.yaml: %s",
+						err.Error()))
+				}
+			} else if fileHelper.Exists("/etc/anise/luet.yaml") {
+				err = rewriteConfigPaths("/etc/anise/luet.yaml")
+				if err != nil {
+					Fatal(fmt.Sprintf(
+						"error on update file /etc/anise/luet.yaml: %s",
+						err.Error()))
 				}
 			}
 
@@ -83,4 +128,89 @@ func newMigrateLuetCommand(cfg *config.AniseConfig) *cobra.Command {
 	}
 
 	return ans
+}
+
+func rewriteConfigPaths(file string) error {
+
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	config := strings.ReplaceAll(string(data),
+		"etc/luet", "etc/anise")
+
+	err = os.WriteFile(file, []byte(config), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func updateRepositoriesPkgs(cfg *config.AniseConfig) error {
+	searchOpts := &wagon.StonesSearchOpts{
+		Matches:          []string{"."},
+		Categories:       []string{"repository"},
+		WithFiles:        true,
+		AndCondition:     true,
+		WithRootfsPrefix: false,
+	}
+
+	// Initialize anise artifact manager
+	manager := installer.NewArtifactsManager(cfg)
+	defer manager.Close()
+	manager.Setup()
+
+	manager.Lock()
+	defer manager.Unlock()
+
+	searcher := wagon.NewSearcherSimple(cfg)
+
+	stonesref, err := searcher.SearchInstalled(searchOpts)
+	searcher.Close()
+	if err != nil {
+		return err
+	}
+
+	for _, stone := range *stonesref {
+
+		prepo := stone.ToPackage()
+
+		InfoC(fmt.Sprintf(":factory: [%s] Update files list...",
+			prepo.HumanReadableString()))
+
+		// Delete existing package without remove filesystem files.
+		err := manager.Database.RemovePackageFiles(prepo)
+		if err != nil {
+			return err
+		}
+
+		newFilesList := []string{}
+
+		for idx := range stone.Files {
+
+			// Drop rootfs path string
+			file := strings.ReplaceAll(stone.Files[idx],
+				"etc/luet", "etc/anise")
+
+			newFilesList = append(newFilesList, file)
+		}
+
+		// Set package Files
+		err = manager.Database.SetPackageFiles(
+			&pkg.PackageFile{
+				PackageFingerprint: prepo.GetFingerPrint(),
+				Files:              newFilesList,
+			},
+		)
+		if err != nil {
+			return fmt.Errorf(
+				"error on update files list of package %s",
+				prepo.HumanReadableString())
+		}
+
+	}
+
+	return nil
 }
