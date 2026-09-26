aa1c5f77fb433f95826f04a12a6d6a5606b64281
Daniele Rondina <geaaru@macaronios.org>
2026-09-26 19:09:04 +0200

    anise-build: Cleanup cli helpers

--- anise-build/cmd/helpers/helpers.go
+++ anise-build/cmd/helpers/helpers.go
@@ -37,24 +37,14 @@ func CreateRegexArray(rgx []string) ([]*regexp.Regexp, error) {
 }
 
 func BindSolverFlags(cmd *cobra.Command) {
-	AniseCfg.Viper.BindPFlag("solver.type", cmd.Flags().Lookup("solver-type"))
-	AniseCfg.Viper.BindPFlag("solver.discount", cmd.Flags().Lookup("solver-discount"))
-	AniseCfg.Viper.BindPFlag("solver.rate", cmd.Flags().Lookup("solver-rate"))
-	AniseCfg.Viper.BindPFlag("solver.max_attempts", cmd.Flags().Lookup("solver-attempts"))
 	AniseCfg.Viper.BindPFlag("solver.implementation", cmd.Flags().Lookup("solver-implementation"))
 }
 
 func SetSolverConfig() (c *AniseSolverOptions) {
 	stype := AniseCfg.Viper.GetString("solver.type")
-	discount := AniseCfg.Viper.GetFloat64("solver.discount")
-	rate := AniseCfg.Viper.GetFloat64("solver.rate")
-	attempts := AniseCfg.Viper.GetInt("solver.max_attempts")
 	implementation := AniseCfg.Viper.GetString("solver.implementation")
 
 	AniseCfg.GetSolverOptions().Type = stype
-	AniseCfg.GetSolverOptions().LearnRate = float32(rate)
-	AniseCfg.GetSolverOptions().Discount = float32(discount)
-	AniseCfg.GetSolverOptions().MaxAttempts = attempts
 	AniseCfg.GetSolverOptions().Implementation = implementation
 
 	if implementation == "" {
@@ -65,9 +55,6 @@ func SetSolverConfig() (c *AniseSolverOptions) {
 
 	return &AniseSolverOptions{
 		Type:           stype,
-		LearnRate:      float32(rate),
-		Discount:       float32(discount),
-		MaxAttempts:    attempts,
 		Implementation: implementation,
 	}
 }
